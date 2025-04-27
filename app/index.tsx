69ba917f5561f5f9600b5e68bb959e51ed451887
jaditi930 <jaditi930@gmail.com>
2025-04-27 17:34:50 +0530

    initial setup

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      className="flex-1 items-center justify-center text-teal-400 bg-orange-900"
+    >
+      <Text className="text-teal-400">Movies App</Text>
+    </View>
+  );
+}
